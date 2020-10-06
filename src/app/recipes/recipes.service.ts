c24920f12bc3ec18cf6017b270ee35952799a5e1
Blessing Makaraba <blessingmakaraba@gmail.com>
2020-10-06 18:37:13 +0100

    Intgrate ionic with Angular

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+      ingredients: ['rice', 'beans'],
+    },
+    {
+      id: 'r2',
+      title: 'Spanish',
+      imageUrl: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+      ingredients: ['good', 'sphaghetti'],
+    },
+  ];
+  constructor() {}
+
+  getRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId) {
+    return { ...this.recipes.find(({ id }) => recipeId === id) };
+  }
+}
