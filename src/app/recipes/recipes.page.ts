f0d73461e016e0dd2590e7a4942a49179b24878c
Blessing Makaraba <blessingmakaraba@gmail.com>
2020-10-08 15:36:56 +0100

    Implement lifecycle hooks that takes care of caching

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { RecipesService } from './recipes.service';
@@ -8,12 +8,32 @@ import { RecipesService } from './recipes.service';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
   recipes: Recipe[];
 
   constructor(private recipesService: RecipesService) {}
 
   ngOnInit() {
+    console.log('init');
+  }
+
+  ionViewWillEnter() {
     this.recipes = this.recipesService.getRecipes();
+    console.log('enter');
+  }
+
+  ionViewDidEnter() {
+    console.log('confrimed');
+  }
+
+  ionViewWillLeave() {
+    console.log('leave');
+  }
+
+  ionViewDidLeave() {
+    console.log('I have left');
+  }
+  ngOnDestroy() {
+    console.log('destroy');
   }
 }
